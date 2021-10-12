d3dc2d18217bc40b71d0536ae86f358ce12cf0f3
Wim Van Deun <7521270+enzzzy@users.noreply.github.com>
2021-10-12 23:22:28 +0200

    fixes issue with Nornir inventory defaults not being applied
    fixes #39

--- nornir_nautobot/plugins/inventory/nautobot.py
+++ nornir_nautobot/plugins/inventory/nautobot.py
@@ -24,7 +24,7 @@ from requests import Session
 logger = logging.getLogger(__name__)
 
 
-def _set_host(data: Dict[str, Any], name: str, groups, host, host_platform) -> Host:
+def _set_host(data: Dict[str, Any], name: str, groups, host, host_platform, defaults: Defaults) -> Host:
     connection_option = {}
     for key, value in data.get("connection_options", {}).items():
         connection_option[key] = ConnectionOptions(
@@ -44,6 +44,7 @@ def _set_host(data: Dict[str, Any], name: str, groups, host, host_platform) -> H
         platform=host_platform,
         data=data,
         groups=groups,
+        defaults=defaults,
         connection_options=connection_option,
     )
 
@@ -161,6 +162,7 @@ class NautobotInventory:  # pylint: disable=R0902
                 groups=host["groups"],
                 host=host,
                 host_platform=getattr(device.platform, "slug", None),
+                defaults=defaults,
             )
 
         return Inventory(hosts=hosts, groups=groups, defaults=defaults)

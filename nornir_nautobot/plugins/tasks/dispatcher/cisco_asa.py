d51979f2d16d926e4aa703382d63ef50ff029703
Jeff Kala <jeff.kala@networktocode.com>
2021-11-10 12:07:10 -0600

    update to use standard inheritance

---
+++ nornir_nautobot/plugins/tasks/dispatcher/cisco_asa.py
@@ -0,0 +1,7 @@
+"""network_importer driver for cisco_asa."""
+
+from .default import NetmikoNautobotNornirDriver as DefaultNautobotNornirDriver
+
+
+class NautobotNornirDriver(DefaultNautobotNornirDriver):
+    """Driver for Cisco ASA."""

7df0e88e8679940a55e8172a1f7d91c19b4ec887
EdificomSA <118285649+EdificomSA@users.noreply.github.com>
2024-07-09 11:03:11 +0200

    Create extreme_exos.py dispatcher
    Netmiko dispatcher for Extreme EXOS

---
+++ nornir_nautobot/plugins/tasks/dispatcher/extreme_exos.py
@@ -0,0 +1,8 @@
+"""nornir dispatcher for Extreme EXOS."""
+
+from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+
+class NetmikoExtremeEXOS(NetmikoDefault):
+    """Collection of Netmiko Nornir Tasks specific to Extreme EXOS devices."""
+
+    config_command = "show configuration detail"

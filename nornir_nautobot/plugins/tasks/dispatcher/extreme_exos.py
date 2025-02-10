785ebef5870898b15cf2d37e148c070ab2bfa202
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-02-10 09:56:39 -0700

    Update nornir_nautobot/plugins/tasks/dispatcher/extreme_exos.py

--- nornir_nautobot/plugins/tasks/dispatcher/extreme_exos.py
+++ nornir_nautobot/plugins/tasks/dispatcher/extreme_exos.py
@@ -3,6 +3,10 @@
 from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
 
 
+class NapalmExtremeEXOS(NapalmDefault):
+    """Collection of Napalm Nornir Tasks specific to ExtremeEXOS devices."""
+
+
 class NetmikoExtremeEXOS(NetmikoDefault):
     """Collection of Netmiko Nornir Tasks specific to Extreme EXOS devices."""
 

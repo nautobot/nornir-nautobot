abc3c27ad4ec25073bada3be39d80e02b551206d
Josef Fuchs <josef.fuchs@pankl.com>
2025-07-02 06:12:14 +0200

    Align with other task plugins

--- nornir_nautobot/plugins/tasks/dispatcher/checkpoint_gaia.py
+++ nornir_nautobot/plugins/tasks/dispatcher/checkpoint_gaia.py
@@ -1,9 +1,12 @@
 """nornir dispatcher for checkpoint_gaia."""
 
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
+from nornir_nautobot.plugins.tasks.dispatcher.default import NapalmDefault, NetmikoDefault
 
 
-class CheckpointGaiaDefault(NetmikoDefault):
+class NapalmCheckpointGaia(NapalmDefault):
+    """Collection of Napalm Nornir Tasks specific to Check Point Gaia devices."""
+
+class NetmikoCheckpointGaia(NetmikoDefault):
     """Collection of Netmiko Nornir Tasks specific to Check Point Gaia devices."""
 
-    config_command = 'clish -c "show configuration"'
+    config_command = '/usr/bin/clish -c "show configuration"'

adf4a7a1af50e118ac2ac17d5a33d2f438b2b6c9
Josef Fuchs <josef.fuchs@pankl.com>
2025-07-02 06:24:32 +0200

    Remove accidentially added dispatchers

--- nornir_nautobot/plugins/tasks/dispatcher/hp_comware.py
+++
@@ -1,9 +0,0 @@
-"""nornir dispatcher for hp_comware devices."""
-
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
-
-
-class HPComwareDefault(NetmikoDefault):
-    """Collection of Netmiko Nornir Tasks specific to HPE Comware 5/7 devices."""
-
-    config_command = "display current-configuration"
--- nornir_nautobot/plugins/tasks/dispatcher/hp_procurve.py
+++
@@ -1,9 +0,0 @@
-"""nornir dispatcher for hp_procurve devices."""
-
-from nornir_nautobot.plugins.tasks.dispatcher.default import NetmikoDefault
-
-
-class HPProcurveDefault(NetmikoDefault):
-    """Collection of Netmiko Nornir Tasks specific to HP Procurve devices."""
-
-    config_command = "show running-config"

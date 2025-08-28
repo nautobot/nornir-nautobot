d26c0cedb61b49142278f13fac87489a8e9d84d6
Joe Wesch <10467633+joewesch@users.noreply.github.com>
2025-08-28 09:53:21 -0500

    Adding get_command_with_prompts to NetmikoDefault (#211)
    * Adds get_command_with_prompts
    
    * Fixes default error code
    
    * Adds changelog fragment
    
    * Ignores pylint errors
    
    * Changes unexpected prompt to raise exception
    
    * Changes output to be consistent with other methods
    
    * Adds types to docstring
    
    * Changes variable name
    
    * Adds full example that uses regex

--- nornir_nautobot/constants.py
+++ nornir_nautobot/constants.py
@@ -245,6 +245,18 @@ ERROR_CODES = {
         error_message=JINJA_ERRORS,
         recommendation="Use a valid jinja filter, common reasons this error occurs include typos or jinja filter is not loaded into the Nautobot worker or web server.",
     ),
+    "E1035": ErrorCode(
+        troubleshooting="Verify that the username and password are correct.",
+        description="The authentication credentials were not accepted.",
+        error_message="Discovered `% Authentication failed` in the output",
+        recommendation="Ensure that the username and password are correct.",
+    ),
+    "E1036": ErrorCode(
+        troubleshooting="Verify that all expected prompts are defined in the prompts dictionary.",
+        description="A prompt was encountered that was not defined in the prompts dictionary.",
+        error_message="No prompt matched for ```\n{last_output}\n```",
+        recommendation="Verify that all expected prompts are defined in the prompts dictionary.",
+    ),
 }
 
 EXCEPTION_TO_ERROR_MAPPER = {
--- nornir_nautobot/plugins/tasks/dispatcher/default.py
+++ nornir_nautobot/plugins/tasks/dispatcher/default.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import logging
 import os
+import re
 import socket
 from typing import Optional
 
@@ -275,6 +276,8 @@ class DispatcherMixin:
             return True, get_error_message("E1029")
         if "% Permission denied for the role" in result_output:
             return True, get_error_message("E1030")
+        if "% Authentication failed" in result_output:
+            return True, get_error_message("E1035")
         return False, ""
 
     @classmethod
@@ -738,7 +741,7 @@ class NetmikoDefault(DispatcherMixin):
                     logger.error(error_msg, extra={"object": obj})
                     raise NornirNautobotException(error_msg)
         except NornirSubTaskError as exc:
-            error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc), "E1016")
+            error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc), "E1014")
             error_msg = get_error_message(error_code, exc=exc)
             logger.error(error_msg, extra={"object": obj})
             raise NornirNautobotException(error_msg)
@@ -792,12 +795,69 @@ class NetmikoDefault(DispatcherMixin):
                         raise NornirNautobotException(error_msg)
                 command_results.update({command: result[0].result})
             except NornirSubTaskError as exc:
-                error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc.result.exception), "E1016")
+                error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc.result.exception), "E1014")
                 error_msg = get_error_message(error_code, exc=exc)
                 logger.error(error_msg, extra={"object": obj})
                 raise NornirNautobotException(error_msg)
 
         return Result(host=task.host, result={"output": command_results})
+
+    @classmethod
+    def get_command_with_prompts(
+        cls,
+        task: Task,
+        logger,
+        obj,
+        command,
+        prompt_responses,
+        regex_flags=re.IGNORECASE,
+        escape_sequence=chr(3),  # Ctrl-C
+        **kwargs,
+    ):  # pylint: disable=too-many-positional-arguments, too-many-locals
+        """A task to run a command on a device and react to resulting prompts.
+
+        Args:
+            task (Task): Nornir Task.
+            logger (logging.Logger): Logger that may be a Nautobot Jobs or Python logger.
+            obj (Device): A Nautobot Device Django ORM object instance.
+            command (str): A command to execute.
+            prompt_responses (dict): A dictionary of regex pattern prompts and responses.
+            regex_flags (re.RegexFlag): Flags to pass to re.search.
+            escape_sequence (str): The escape sequence to send if no prompt is matched.
+            kwargs (dict): Additional keyword arguments to pass to Netmiko.
+        """
+        logger.debug(f"Executing get_command_with_prompts for {task.host.name} on {task.host.platform}")
+
+        net_connect = task.host.get_connection("netmiko", task.nornir.config)
+        base_prompt = net_connect.find_prompt()
+        # We disable stripping the prompt to detect if the command is complete
+        last_output = net_connect.send_command_timing(command, strip_prompt=False, **kwargs)
+        full_output = last_output
+        while True:
+            failed, error_msg = cls._has_hidden_errors(last_output)
+            if failed:
+                logger.error(error_msg, extra={"object": obj})
+                raise NornirNautobotException(error_msg)
+
+            if last_output.endswith(base_prompt):
+                logger.debug("Base prompt found. Command complete.")
+                break
+
+            for prompt, response in prompt_responses.items():
+                if re.search(prompt, last_output, regex_flags):
+                    logger.debug(f"Matched prompt: {prompt}")
+                    last_output = net_connect.send_command_timing(response, strip_prompt=False, **kwargs)
+                    full_output += last_output
+                    # If we matched a prompt, we need to start the loop again to handle any subsequent prompts
+                    break
+            else:
+                error_msg = get_error_message("E1036", last_output=last_output)
+                logger.error(error_msg)
+                logger.debug(f"Sending escape sequence: {escape_sequence}")
+                last_output = net_connect.send_command_timing(escape_sequence, **kwargs)
+                raise NornirNautobotException(error_msg)
+
+        return Result(host=task.host, result={"output": {command: full_output}})
 
 
 class ScrapliDefault(DispatcherMixin):
@@ -894,7 +954,7 @@ class ScrapliDefault(DispatcherMixin):
                     raise NornirNautobotException(error_msg)
                 command_results.update({command: result[0].result})
             except NornirSubTaskError as exc:
-                error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc.result.exception), "E1016")
+                error_code = EXCEPTION_TO_ERROR_MAPPER.get(type(exc.result.exception), "E1014")
                 error_msg = get_error_message(error_code, exc=exc)
                 logger.error(error_msg, extra={"object": obj})
                 raise NornirNautobotException(error_msg)
